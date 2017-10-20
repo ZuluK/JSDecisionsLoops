ff35354e0949c972d723ed401159464d00309f66
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-10-19 20:07:07 -0700

    First Commit

--- js/main.js
+++ js/main.js
@@ -39,6 +39,35 @@ var hotItems = document.querySelectorAll('li.hot'); //Store nodelist in array
 
 if (hotItems.length > 0){//If it contains items
   for (var i = 0; i < hotItems.length; i++){//loop through each item
-    hotItems[i].className = 'cool';//change value of class attribute 
+    hotItems[i].className = 'cool';//change value of class attribute
   }
 }
+
+
+
+//working within the DOM in JS
+//Accessing and changing a text node
+
+var itemTwo = document.getElementById('one'); // get first list itemTwo
+var elText = itemTwo.firstChild.nodeValue; //get it's text content
+elText = elText.replace('pinenuts','kale'); //change text from pinenuts to kale
+itemTwo.firstChild.nodeValue = elText; //update the node value
+
+
+//access and update text with textcontent
+//One of the issues with texContent property is that Internet Explorer didn't support in unit IE9
+//All other major browsers support it
+
+
+//Accessing Text only
+var firstItem = document.getElementById('one'); //find first item list
+var showTextContent = firstItem.textContent;//Get value of textContent
+var showInnerText = firstItem.innerHTML; //Get value of inner Text
+
+//Show the context of these two properties at the end of the list
+var msg = '<p>'textContent: ' + ' + showTextContent + '</p>';
+    msg += '<p>'innerHTML + showInnerText + '</p>';
+var el = document.getElementById('scriptResults');
+el.innerHTML = msg;
+
+firstItem.textContent = 'sourdough bread';     
